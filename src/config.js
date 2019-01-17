2699c245541e055a86b9b3dd20ff98a429156b13
Simply007 <chrastina.ondra@gmail.com>
2019-01-17 13:30:20 +0100

    prepare for version 2.3.0

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const customTrackingHeader = {
   header: 'X-KC-SOURCE',
-  value: 'gatsby-source-kentico-cloud;2.3.0-beta2',
+  value: 'gatsby-source-kentico-cloud;2.3.0',
 };
 
 exports.customTrackingHeader = customTrackingHeader;
