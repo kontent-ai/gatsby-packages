6c6704454fa347bec4c125867833d624b1b7abac
Simply007 <chrastina.ondra@gmail.com>
2019-02-19 11:20:44 +0100

    increate version

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const customTrackingHeader = {
   header: 'X-KC-SOURCE',
-  value: 'gatsby-source-kentico-cloud;3.0.0-beta2',
+  value: 'gatsby-source-kentico-cloud;3.0.0-beta3',
 };
 
 module.exports = {
