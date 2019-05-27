9fe385ef8cfa9b5557075191924aeac7919ff97d
Martin Makarský <martinm@kentico.com>
2019-05-27 12:19:43 +0200

    Remove beta versioning.

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const customTrackingHeader = {
   header: 'X-KC-SOURCE',
-  value: 'gatsby-source-kentico-cloud;3.1.0-beta1',
+  value: 'gatsby-source-kentico-cloud;3.1.0',
 };
 
 module.exports = {
