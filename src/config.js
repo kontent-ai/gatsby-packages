5b1eb562c86b42d2623e75569cc07cef15c64402
Ondřej Chrastina <chrastina.ondra@gmail.com>
2019-10-21 07:36:18 +1100

    raise beta version

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const customTrackingHeader = {
   header: 'X-KC-SOURCE',
-  value: '@kentico/gatsby-source-kontent;4.0.0-beta1',
+  value: '@kentico/gatsby-source-kontent;4.0.0-beta2',
 };
 
 module.exports = {
