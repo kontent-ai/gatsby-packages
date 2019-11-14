6d64915b1b69d631d0a2cbeaa39e6ccd06b8ce71
Simply007 <chrastina.ondra@gmail.com>
2019-11-14 13:42:20 +0100

    fix linked items for linked items in linked items element as well as in rich text element

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const customTrackingHeader = {
   header: 'X-KC-SOURCE',
-  value: '@kentico/gatsby-source-kontent;4.0.0-beta3',
+  value: '@kentico/gatsby-source-kontent;4.0.0-beta4',
 };
 
 module.exports = {
