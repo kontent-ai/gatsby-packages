1494c3b04f807f61509852313c9b47184b5f6542
Simply007 <chrastina.ondra@gmail.com>
2019-12-05 09:45:16 +0100

    lint

--- src/__tests__/preview/performUpdate.spec.js
+++ src/__tests__/preview/performUpdate.spec.js
@@ -1,51 +1,6 @@
 const testItemNode = require('./test-item-node');
-const itemResponseUpdateText = require('./item-response-updated-text');
 
 const { sourceNodes } = require('../../gatsby-node');
-
-// describe('Gatsby Preview update', () => {
-//   describe('text element', () => {
-//     it('updated correctly', async () => {
-//       const api = {
-//         actions: {
-//           createNode: jest.fn(),
-//           createTypes: jest.fn(),
-//           touchNode: jest.fn(),
-//         },
-//         createNodeId: jest.fn(() => '1bdc8830-979d-57b2-a2a3-501486825575'),
-//         schema: {
-//           buildObjectType: jest.fn(),
-//         },
-//         webhookBody: {
-//           message: {
-//             operation: 'update',
-//             elementCodenames: ['text'],
-//             selectedLanguage: 'default',
-//           },
-//           data: {
-//             response: itemResponseUpdateText,
-//           },
-//         },
-//         getNodes: jest.fn(() => [testItemNode]),
-//       };
-
-//       const pluginConfig = {
-//         deliveryClientConfig: {
-//           projectId: 'dummyProject',
-//           typeResolvers: [],
-//         },
-//         languageCodenames: ['default'],
-//       };
-
-//       await sourceNodes(api, pluginConfig);
-
-//       expect(api.actions.createNode.mock.calls)
-//         .toHaveLength(1);
-//       expect(api.actions.createNode.mock.calls[0])
-//         .toMatchSnapshot();
-//     });
-//   });
-// });
 
 describe('Gatsby Preview update', () => {
   it.each([
@@ -55,8 +10,10 @@ describe('Gatsby Preview update', () => {
     ['custom element', './item-response-updated-custom'],
     ['url slug element', './item-response-updated-url-slug'],
     ['asset element', './item-response-updated-asset'],
-    ['linked items element', './item-response-updated-linked-items'], // ___NODES NEEDS UPDATE
-    ['rich text element', './item-response-updated-rich-text'], // ___NODES NEED UPDATE
+    // ___NODES NEEDS UPDATE
+    ['linked items element', './item-response-updated-linked-items'],
+    // ___NODES NEED UPDATE
+    ['rich text element', './item-response-updated-rich-text'],
   ])('%s', async (_, updateResponse) => {
     const response = require(updateResponse);
 
