6b49bb5ed2109d921b68c9a9b17c67fea4df5ff0
Ondřej Chrastina <9218736+Simply007@users.noreply.github.com>
2020-01-21 17:35:35 +0100

    Prepare for major version release

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const customTrackingHeader = {
   header: 'X-KC-SOURCE',
-  value: '@kentico/gatsby-source-kontent;4.3.0-beta1',
+  value: '@kentico/gatsby-source-kontent;4.3.0',
 };
 
 module.exports = {
