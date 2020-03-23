6aa3ca29faa5eff8df30680bfabae67c3cffdfca
ondrejch <ondrejch@kentico.com>
2020-03-23 10:57:18 +0100

    cleanup ald taxonomies package

--- packages/gatsby-source-kontent-taxonomies/gatsby-node.ts
+++
@@ -1,4 +0,0 @@
-import { kontentTaxonomiesCreateSchemaCustomization } from './src/createSchemaCustomization.taxonomies';
-import { kontentTaxonomiesSourceNodes } from './src/sourceNodes.taxonomies';
-
-export { kontentTaxonomiesCreateSchemaCustomization as createSchemaCustomization, kontentTaxonomiesSourceNodes as sourceNodes };
