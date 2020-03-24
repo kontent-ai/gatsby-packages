e32d073f1df7e1f7b52f3b8b7a69202e2376f7a1
ondrejch <ondrejch@kentico.com>
2020-03-24 10:14:50 +0100

    lint and npm publish config

--- packages/gatsby-source-kontent/gatsby-node.ts
+++ packages/gatsby-source-kontent/gatsby-node.ts
@@ -1,16 +1,25 @@
-import { CustomPluginOptions, CustomCreateSchemaCustomizationArgs } from "./src/types";
-import { SourceNodesArgs } from "gatsby";
+import {
+  CustomPluginOptions,
+  CustomCreateSchemaCustomizationArgs,
+} from './src/types';
+import { SourceNodesArgs } from 'gatsby';
 
 import { kontentItemsCreateSchemaCustomization } from './src/createSchemaCustomization.items';
 import { kontentItemsSourceNodes } from './src/sourceNodes.items';
-import { kontentTaxonomiesCreateSchemaCustomization } from "./src/createSchemaCustomization.taxonomies";
-import { kontentTaxonomiesSourceNodes } from "./src/sourceNodes.taxonomies";
-import { kontentTypesCreateSchemaCustomization } from "./src/createSchemaCustomization.types";
-import { kontentTypesSourceNodes } from "./src/sourceNodes.types";
+import { kontentTaxonomiesCreateSchemaCustomization } from './src/createSchemaCustomization.taxonomies';
+import { kontentTaxonomiesSourceNodes } from './src/sourceNodes.taxonomies';
+import { kontentTypesCreateSchemaCustomization } from './src/createSchemaCustomization.types';
+import { kontentTypesSourceNodes } from './src/sourceNodes.types';
 
-export { kontentItemsCreateSchemaCustomization as createSchemaCustomization, kontentItemsSourceNodes as sourceNodes };
+export {
+  kontentItemsCreateSchemaCustomization as createSchemaCustomization,
+  kontentItemsSourceNodes as sourceNodes,
+};
 
-exports.createSchemaCustomization = async (api: CustomCreateSchemaCustomizationArgs, pluginConfig: CustomPluginOptions): Promise<void> => {
+exports.createSchemaCustomization = async (
+  api: CustomCreateSchemaCustomizationArgs,
+  pluginConfig: CustomPluginOptions,
+): Promise<void> => {
   await kontentItemsCreateSchemaCustomization(api, pluginConfig);
   if (pluginConfig.includeTaxonomies) {
     await kontentTaxonomiesCreateSchemaCustomization(api);
@@ -18,9 +27,12 @@ exports.createSchemaCustomization = async (api: CustomCreateSchemaCustomizationA
   if (pluginConfig.includeTypes) {
     await kontentTypesCreateSchemaCustomization(api);
   }
-}
+};
 
-exports.sourceNodes = async (api: SourceNodesArgs, pluginConfig: CustomPluginOptions): Promise<void> => {
+exports.sourceNodes = async (
+  api: SourceNodesArgs,
+  pluginConfig: CustomPluginOptions,
+): Promise<void> => {
   await kontentItemsSourceNodes(api, pluginConfig);
   if (pluginConfig.includeTaxonomies) {
     await kontentTaxonomiesSourceNodes(api, pluginConfig);
@@ -28,6 +40,4 @@ exports.sourceNodes = async (api: SourceNodesArgs, pluginConfig: CustomPluginOpt
   if (pluginConfig.includeTypes) {
     await kontentTypesSourceNodes(api, pluginConfig);
   }
-}
-
-
+};
