56470ea799c0c5d6de2a5e5112d7155f4886105f
ondrejch <ondrejch@kentico.com>
2020-04-29 18:06:13 +0200

    check kontent structure of the webhook

--- packages/gatsby-source-kontent/src/webhookProcessor.ts
+++ packages/gatsby-source-kontent/src/webhookProcessor.ts
@@ -3,32 +3,25 @@ import { CustomPluginOptions, KontentTaxonomy, KontentItem, KontentType } from "
 import * as client from "./client";
 import { addPreferredLanguageProperty, alterRichTextElements, getKontentItemLanguageVariantArtifact } from "./sourceNodes.items";
 import { getKontentItemNodeStringForId, getKontentTaxonomyTypeName, getKontentTypeTypeName } from "./naming";
+import * as _ from "lodash";
+import { IWebhookDeliveryResponse } from '@kentico/kontent-webhook-helper';
 
-interface WebhookBody {
-  data: WebhookData;
-  message: WebhookBodyMessage;
-}
 
-interface WebhookBodyMessage {
-  api_name: string;
-  type: string;
-  operation: string;
-}
+const hasKontentWebhookStructure = (api: SourceNodesArgs, projectId: string): boolean => {
+  const webhookProjectId = _.get(api.webhookBody, 'message.project_id');
+  if (webhookProjectId !== projectId) {
+    return false;
+  }
 
-interface WebhookData {
-  items: {
-    id: string;
-    codename: string;
-    language: string;
-    type: string;
-  }[];
+  const response = api.webhookBody as IWebhookDeliveryResponse;
+  return !!response;
 }
 
 const handleUpsertItem = async (
   api: SourceNodesArgs,
   pluginConfig: CustomPluginOptions
 ): Promise<string[]> => {
-  const itemInfo = (api.webhookBody as WebhookBody)?.data.items[0];
+  const itemInfo = (api.webhookBody as IWebhookDeliveryResponse)?.data.items[0];
 
   if (!pluginConfig.languageCodenames.includes(itemInfo.language)) {
     api.reporter.verbose(`Cant find specified language ${itemInfo.language} in plugin configuration`);
@@ -65,7 +58,7 @@ const handleDeleteItem = async (
   pluginConfig: CustomPluginOptions
 ): Promise<string[]> => {
 
-  const itemInfo = (api.webhookBody as WebhookBody)?.data.items[0];
+  const itemInfo = (api.webhookBody as IWebhookDeliveryResponse)?.data.items[0];
 
   if (!pluginConfig.languageCodenames.includes(itemInfo.language)) {
     api.reporter.verbose(`Cant find specified language ${itemInfo.language} in plugin configuration`);
@@ -108,34 +101,54 @@ const handleIncomingWebhook = async (
   pluginConfig: CustomPluginOptions,
   itemTypes: string[],
 ): Promise<void> => {
-  const webhook = api.webhookBody as WebhookBody;
+
+  if (!hasKontentWebhookStructure(api, pluginConfig.projectId)) {
+    api.reporter.verbose('Webhook ignored - webhook  is not supported');
+    return;
+  }
+
+  const webhook = api.webhookBody as IWebhookDeliveryResponse;
   const message = webhook.message;
 
+
   api.reporter.verbose(`Handling ${message.operation} from ${message.api_name} API`);
-  if(webhook.data.items.length > 1){
+  if (webhook.data.items.length > 1) {
     api.reporter.warn(`Webhook contains more than one item! - contains (${webhook.data.items.length})`)
   }
 
   const processedItemIds: string[] = [];
-  if(message.api_name === 'delivery_preview') {
+  // TODO fix once pull request is merged and release: https://github.com/Kentico/kontent-webhook-helper-js/pull/1
+  if (message.api_name as string === 'delivery_preview') {
+    
+    // TODO: Webhook header signature (once headers are available)
+    // use signatureHelper '@kentico/kontent-webhook-helper'
+    // https://github.com/gatsbyjs/gatsby/issues/23593
+
     if (message.operation === "upsert") {
-      // TODO question - could create contains more than one item?
+      // TODO question - could upsert contains more than one item?
       const processedIds = await handleUpsertItem(api, pluginConfig);
       processedItemIds.concat(processedIds);
     }
-  
+
     if (message.operation === "archive") {
+      // TODO question - could upsert contains more than one item?
       const processedIds = await handleDeleteItem(api, pluginConfig);
       processedItemIds.concat(processedIds);
     }
   } else if (message.api_name === 'delivery_production') {
+
+    // TODO: Webhook header signature (once headers are available)
+    // use signatureHelper '@kentico/kontent-webhook-helper'
+    // https://github.com/gatsbyjs/gatsby/issues/23593
+
     if (message.operation === "publish") {
-      // TODO question - could create contains more than one item?
+      // TODO question - could publish contains more than one item?
       const processedIds = await handleUpsertItem(api, pluginConfig);
       processedItemIds.concat(processedIds);
     }
-  
+
     if (message.operation === "unpublish") {
+      // TODO question - could unpublish contains more than one item?
       const processedIds = await handleDeleteItem(api, pluginConfig);
       processedItemIds.concat(processedIds);
     }
