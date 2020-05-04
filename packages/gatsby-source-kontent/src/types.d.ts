04d010ebd2468c88f49627100fa29e88596ed384
ondrejch <ondrejch@kentico.com>
2020-05-04 15:21:54 +0200

    define all possible element value shapes in typescript definition

--- packages/gatsby-source-kontent/src/types.d.ts
+++ packages/gatsby-source-kontent/src/types.d.ts
@@ -32,10 +32,30 @@ interface PluginNamingConfiguration {
 interface KontentItemElement {
   name: string;
   type: string;
-  value: string | number | string[];
+  value: string | number | string[] | AssetElementValue[] | MultipleChoiceOption[] | TaxonomyTerm[];
   images: { [key: string]: RichTextElementImage } | RichTextElementImage[];
   links: { [key: string]: RichTextElementLink } | RichTextElementLink[];
   modular_content: string[];
+}
+
+interface AssetElementValue {
+  name: string;
+  type: string;
+  size: number;
+  description: string;
+  url: string;
+  width?: number;
+  height?: number;
+}
+
+interface MultipleChoiceOption {
+  name: string;
+  codename: string;
+}
+
+interface TaxonomyTerm {
+  name: string;
+  codename: string;
 }
 
 interface RichTextElementImage {
