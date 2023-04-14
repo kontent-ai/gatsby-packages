05a5a2799d052d7ea3f2cb002ce51f99bd6d7fff
Ondřej Chrastina <ondrejch@kontent.ai>
2023-04-14 15:04:56 +0200

    Ignore browserparser

--- examples/resolution/gatsby-node.js
+++ examples/resolution/gatsby-node.js
@@ -95,3 +95,19 @@ exports.createPages = async ({ graphql, actions }) => {
     })
   });
 }
+
+// https://www.gatsbyjs.com/docs/debugging-html-builds/#fixing-third-party-modules
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html" || stage === "develop-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /RichTextBrowserParser/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
